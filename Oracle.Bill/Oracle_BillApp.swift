483846e086369f3b817231762b7940f5ee49c9e1
Altan Duman <halicedu@gmail.com>
2026-06-22 11:13:25 +0300

    Initial Commit

---
+++ Oracle.Bill/Oracle_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oracle_BillApp.swift
+//  Oracle.Bill
+//
+//  Created by Altan Duman on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Oracle_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
